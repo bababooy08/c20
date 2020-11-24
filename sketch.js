1834fef959376d71199e2a87ce6d4036dc92ca36
bababooy08 <74669724+bababooy08@users.noreply.github.com>
2020-11-24 15:49:11 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var a,b;
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  b=createSprite(200,100,20,20);
+  a.shapeColor ='green';
+  b.shapeColor ='green';
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  a.x = World.mouseX; a.y = World.mouseY; console.log(a.x-b.x);
+   if(a.x-b.x < a.width/2 + b.width/2 && b.x-a.x < a.width/2 + b.width)
+   { a.shapeColor = "red"; b.shapeColor = "red"; }
+    else
+  { a.shapeColor = "green"; b.shapeColor = "green"; }
+
+
+
+
+
+
+
+
+  drawSprites();
+}
